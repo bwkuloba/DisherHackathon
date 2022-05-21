2ff9af97939b2b7647d19933869889ebeac386b3
bwkuloba <bwkuloba@gmail.com>
2022-05-21 15:26:20 -0700

    Initial Commit

---
+++ DisherHackathon/DisherHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisherHackathonApp.swift
+//  DisherHackathon
+//
+//  Created by Basil Kuloba on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct DisherHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
